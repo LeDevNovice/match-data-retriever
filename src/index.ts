dcd85c5b492a4ed8e9242d7ba0923d4ee344674c
GregorySaison <gsaison@apizr.fr>
2024-09-18 20:22:42 +0200

    feat: add main function to call

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import retrieveHistoricalOdds from './utils/retrieveHistoricalOdds';
+import checkCommandLineArgs from './utils/checkCommandLineArgs';
+import { processMatchesInBatches } from './controllers/batchProcessor';
+
+async function main(): Promise<void> {
+  try {
+    const { country, league, year } = checkCommandLineArgs();
+    await retrieveHistoricalOdds(country, league, year);
+
+    const MAX_API_CALLS_PER_DAY = 100;
+    const API_CALLS_PER_MATCH = 10;
+    const MAX_MATCHES_PER_DAY = Math.floor(MAX_API_CALLS_PER_DAY / API_CALLS_PER_MATCH);
+    const BATCH_SIZE = MAX_MATCHES_PER_DAY;
+
+    await processMatchesInBatches(BATCH_SIZE);
+  } catch (error) {
+    console.error('Unexpected error occurred:', error);
+    process.exit(1);
+  }
+}
+
+main();
