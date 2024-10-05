1ddbf4ef33ad39aff2026ffd0d4363689ab0895d
LeDevNovice <gregory.saison@proton.me>
2024-10-05 15:36:27 +0200

    fix: add missing interfaces elements

--- src/models/interfaces/matchOddsData.interface.ts
+++ src/models/interfaces/matchOddsData.interface.ts
@@ -5,6 +5,185 @@ export interface BookmakerOdds {
   awayWin: number;
 }
 
+interface LineupData {
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+    colors: {
+      player: {
+        primary: string;
+        number: string;
+        border: string;
+      };
+      goalkeeper: {
+        primary: string;
+        number: string;
+        border: string;
+      };
+    };
+  };
+  coach: {
+    id: number;
+    name: string;
+    photo: string;
+  };
+  formation: string;
+  startXI: Array<{
+    player: {
+      id: number;
+      name: string;
+      number: number;
+      pos: string;
+      grid: string | null;
+    };
+  }>;
+  substitutes: Array<{
+    player: {
+      id: number;
+      name: string;
+      number: number;
+      pos: string;
+      grid: string | null;
+    };
+  }>;
+}
+
+interface InjuriesData {
+  player: {
+    id: number;
+    name: string;
+    photo: string;
+    type: string;
+    reason: string;
+  };
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+  };
+  fixture: {
+    id: number;
+    timezone: string;
+    date: string;
+    timestamp: number;
+  };
+  league: {
+    id: number;
+    season: number;
+    name: string;
+    country: string;
+    logo: string;
+    flag: string;
+  };
+}
+
+interface StatisticsData {
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+  };
+  statistics: Array<{
+    type: string;
+    value: number | string | null;
+  }>;
+}
+
+interface EventsData {
+  time: {
+    elapsed: number;
+    extra: number | null;
+  };
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+  };
+  player: {
+    id: number;
+    name: string;
+  };
+  assist?: {
+    id: number | null;
+    name: string | null;
+  };
+  type: string;
+  detail: string;
+  comments: string | null;
+}
+
+interface PlayersData {
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+    update: string;
+  };
+  players: Array<{
+    player: {
+      id: number;
+      name: string;
+      photo: string;
+    };
+    statistics: Array<{
+      games: {
+        minutes: number | null;
+        number: number;
+        position: string;
+        rating: string | null;
+        captain: boolean;
+        substitute: boolean;
+      };
+      offsides?: number | null;
+      shots: {
+        total: number | null;
+        on: number | null;
+      };
+      goals: {
+        total: number | null;
+        conceded: number | null;
+        assists: number | null;
+        saves: number | null;
+      };
+      passes: {
+        total: number | null;
+        key: number | null;
+        accuracy: string | null;
+      };
+      tackles: {
+        total: number | null;
+        blocks: number | null;
+        interceptions: number | null;
+      };
+      duels: {
+        total: number | null;
+        won: number | null;
+      };
+      dribbles: {
+        attempts: number | null;
+        success: number | null;
+        past: number | null;
+      };
+      fouls: {
+        drawn: number | null;
+        committed: number | null;
+      };
+      cards: {
+        yellow: number;
+        red: number;
+      };
+      penalty: {
+        won: number | null;
+        commited: number | null;
+        scored: number;
+        missed: number;
+        saved: number | null;
+      };
+    }>;
+  }>;
+}
+
 export interface MatchOddsData {
   day: string;
   date: string;
@@ -15,9 +194,9 @@ export interface MatchOddsData {
   season: string;
   score: string;
   odds: BookmakerOdds[];
-  // lineup: LineupData;
-  // injuries: InjuriesData;
-  // statistics: StatisticsData;
-  // events: EventsData;
-  // players: PlayersData;
+  lineup: LineupData;
+  injuries: InjuriesData;
+  statistics: StatisticsData;
+  events: EventsData;
+  players: PlayersData;
 }
